bca6ae4d490f172ff37d4ecfb65482c2365c60b0
fajarfrdiansyah <fajarfirdiansyah21@gmail.com>
2022-03-29 13:41:31 +0700

    secondd commit

--- script.js
+++ script.js
@@ -5,3 +5,87 @@ numbers.forEach((number) => {
         console.log(event.target.value)
     })
 })
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+
+const numbers = document.querySelectorAll(".number")
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        updateScreen(event.target.value)
+    })
+})
+
+let prevNumber = ''
+let CalculationOperator = ''
+let currentNumber = '0'
+
+const inputNumber = (number) => {
+    currentNumber = number
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+
+const inputNumber = (number) => {
+    currentNumber += number
+}
+
+const inputNumber = (number) => {
+    if (currentNumber === '0') {
+        currentNumber = number 
+    } else {
+        currentNumber += number
+    }
+}
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        console.log(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    prevNumber = currentNumber 
+    CalculationOperator = operator
+    currentNumber = ''
+}
+
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click', () => {
+    console.log('equal button is pressed')
+})
+
+const calculate = () => {
+    let result = ''
+    switch(CalculationOperator) {
+        case "+":
+            result = prevNumber + currentNumber
+            break
+        case "-":
+            result = prevNumber - currentNumber
+            break
+        case "*":
+            result = prevNumber * currentNumber
+            break
+        case "/":
+            result = prevNumber / currentNumber
+            break
+        default:
+            break        
+    }
+    currentNumber = result
+    CalculationOperator = ''
+}
+
